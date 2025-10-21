477d9b3c12647f9a0099edf936675933d339a521
milos.brecher <milos.brecher@gmail.com>
2025-10-22 00:39:35 +0200

    chore: update FrontErrorPresenter block from skeleton

--- app/Modules/Error/FrontError/FrontErrorPresenter.php
+++ app/Modules/Error/FrontError/FrontErrorPresenter.php
@@ -5,20 +5,15 @@ declare(strict_types=1);
 namespace App\Modules\Error\FrontError;
 
 use App\Layout\Presenter\FrontLayout;
-use App\Layout\Presenter\Types\FrontLayoutTemplate;
-use App\Modules\Error\RequestError\RequestError;
-use App\Modules\Error\RequestError\RequestErrorTemplate;
+use App\Modules\Error\RequestError\RequestErrorTrait;
 use Bite\Attributes\Error;
 use Bite\Presenter\AbstractPresenter;
-use Bite\Presenter\Traits\BaseCanonization;
+use Bite\Presenter\Traits\Base\BaseCanonization;
 
-/**
- * @property  RequestErrorTemplate|FrontLayoutTemplate $template
- */
 #[Error]
 final class FrontErrorPresenter extends AbstractPresenter
 {
-    use RequestError;
     use BaseCanonization;
     use FrontLayout;
+    use RequestErrorTrait;
 }
