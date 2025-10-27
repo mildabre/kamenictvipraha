80506eb66cff419d943d1aa76018e62ce9485860
milos.brecher <milos.brecher@gmail.com>
2025-10-27 15:56:08 +0100

    refactor: ContentPresenter, change namespace Modules to Presentation

--- app/Presentation/Front/Content/ContentPresenter.php
+++ app/Presentation/Front/Content/ContentPresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Modules\Front\Content;
+namespace App\Presentation\Front\Content;
 
 use App\Layout\Components\FrontMenu;
 use App\Layout\Presenter\FrontLayout;
