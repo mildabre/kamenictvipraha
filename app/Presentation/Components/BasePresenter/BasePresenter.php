ca4d27dad1207c43b9a2a1a0510767ffe6480172
milos.brecher <milos.brecher@gmail.com>
2026-05-06 23:52:11 +0200

    feat: BasePresenter, add LatteFunctions

--- app/Presentation/Components/BasePresenter/BasePresenter.php
+++ app/Presentation/Components/BasePresenter/BasePresenter.php
@@ -6,8 +6,10 @@ namespace App\Presentation\Components\BasePresenter;
 
 use Bite\Presenter\AbstractPresenter;
 use Bite\Presenter\Traits\Base\BaseCanonization;
+use Bite\Presenter\Traits\Base\LatteFunctions;
 
 abstract class BasePresenter extends AbstractPresenter
 {
     use BaseCanonization;
+    use LatteFunctions;
 }
